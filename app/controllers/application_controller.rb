5e7bbb133b1b46483f6c0baea371977d3a87af9b
massarian <hello74291211@gmail.com>
2020-03-05 20:47:39 +0900

    conflict2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,11 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'saisyukadai' && password == '1028'
     end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
   end
 end
