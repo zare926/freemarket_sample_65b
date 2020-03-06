9781bce99cf87c1cb4319f8ee0a303d6f3dbc462
hikarikawa1028 <okmc7335@yahoo.co.jp>
2020-03-07 07:16:38 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth, if: :production?
   before_action :basic_auth
 
   private
 
+  def production?
+    Rails.env.production?
+  end
+
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'saisyukadai' && password == '1028'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
