5e3cb489b7d8d307e606655638bb6ed557f679ac
hikarikawa1028 <okmc7335@yahoo.co.jp>
2020-03-21 17:38:01 +0900

    1

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,16 +1,5 @@
 $(function() {
-  $('.js-form').on('submit', function(e) {
-    e.preventDefault();
-    var todo = $('.js-form__text-field').val();
-    $.ajax({
-      type: 'POST',
-      url: '/items.json',
-      data: {
-        todo: {
-          content: todo
-        }
-      },
-      dataType: 'json'
-    })
+  $(document).ready(function() {
+    alert('クリックされました');
   });
-});
+})
