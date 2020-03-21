ad393dca9b6b41afbdb5fdb3ce6476b934d569bc
yuzoyuzo0716 <y-ishihara@ito-fashion.co.jp>
2020-03-21 16:31:53 +0900

    coding pay.jp2

--- app/controllers/credit_card_controller.rb
+++ app/controllers/credit_card_controller.rb
@@ -3,22 +3,9 @@ class CreditCardController < ApplicationController
   require "payjp"
 
   def new
-    card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exusts?
+    # credit_card = Credit_card.where(user_id: current_user.id)
+    # redirect_to action: "show" if card.exusts?
   end
-
-  def payjp
-    Payjp.api_key = "sk_test_bd8c841107b0a0f966a79cc5"
-    if params['payjp-token'].blank?
-      redirect_to action: "new"
-    begin
-      customer = 
-    rescue => exception
-      
-    else
-      
-    end
-  
 
   def show
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 2020_03_07_073512) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "postcode", null: false
-    t.integer "user"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "ancestry"
