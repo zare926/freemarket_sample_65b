c5a036741a516b5f0373eac2f1312c30b933b2a4
hikarikawa1028 <okmc7335@yahoo.co.jp>
2020-03-22 01:04:52 +0900

    １

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,5 +1,5 @@
-$(function() {
-  $(document).ready(function() {
-    alert('クリックされました');
+$(document).on("turbolinks:load",function() {
+  $('.top__main__center__img-uplode__box__name-box__product').hover(function(){
+    $('商品名').fadeOut();
   });
-})
+});
