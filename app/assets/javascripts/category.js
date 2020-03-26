6c065d54492af4432ef6a595401ed5b459864488
n-ikeda-94 <59464339+n-ikeda-94@users.noreply.github.com>
2020-03-27 05:57:28 +0900

    hobberAction(js)

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,13 +1,58 @@
+var key = ""
 $(function(){
+    // ページ読み込み時
+  $(document).ready(function(){
+    $('.category-first').hide();
+    $('.category-second').hide();
+    $('.category-third').hide();
+  });
+
+  // カテゴリータブホバー時
   $('.listsLeft__item--first').hover(function(){
-    var html = `
-                <ul class="category__tree">
-                  <li class="category__tree__item">レディース</li>
-                  <li class="category__tree__item">メンズ</li>
-                </ul>
-                `
-    $('.category').append(html);
+    $('.category-first').show();
   }, function(){
-    $('.category').empty();
+    $('.category-first').hide();
+    $('.category-second').hide();
+    $('.category-third').hide();
+  })
+
+  // 第一階層ホバー時
+  $('.category-first__tree').hover(function(){
+    key = this.id
+    $('.category-second').show();
+    $('.category-second__tree').each(function(i,e){
+      if ($(e).attr('parent_id') == key){
+      } else{
+        $(e).hide();
+      }
+    });
+    $('.category-third__tree').each(function(i,e){
+      $(e).show();
+    });
+    $('.category-third').hide();
+  }, function(){
+    $('.category-second').hide();
+  })
+
+  // 第二階層ホバー時
+  $('.category-second__tree').hover(function(){
+    key = this.id
+    $('.category-second').show();
+    $('.category-third').show();
+    $('.category-third__tree').each(function(i,e){
+      $(e).show();
+      if ($(e).attr('parent_id') == key){
+      } else{
+        $(e).hide();
+      }
+    });
+  }, function(){
+
+  })
+
+  // 第三階層ホバー時
+  $('.category-third').hover(function(){
+    $('.category-third').show();
+  }, function(){
   })
 })
