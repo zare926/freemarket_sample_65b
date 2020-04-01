0491a3d84f19fd9a89dc9685f8cc3dfdd2b328fd
massarian <hello74291211@gmail.com>
2020-04-01 21:25:52 +0900

    マイグレーションファイル等データベース関連

--- db/migrate/20200229100055_create_addresses.rb
+++ db/migrate/20200229100055_create_addresses.rb
@@ -1,8 +1,17 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.integer :postcode, null: false
-      t.integer :user
+      t.string :address_family_name,                  null: false
+      t.string :address_first_name,                   null: false
+      t.string :address_family_name_kana,             null: false
+      t.string :address_first_name_kana,              null: false
+      t.string :postcode, null: false
+      t.string :prefecture, null: false
+      t.string :address_city, null: false
+      t.string :address_block, null: false
+      t.string :address_building
+      t.string :phone_number
+      t.references :user
       t.timestamps
     end
   end
