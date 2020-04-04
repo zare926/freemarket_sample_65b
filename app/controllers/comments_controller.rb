46db1df09a36d64cb04668029d8dc8904bd2200b
zare926 <ilove.boc926@gmail.com>
2020-04-04 17:29:49 +0900

    serveritemsshow18

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(text: comment_params[:text])
+    @comment = Comment.create(text: comment_params[:text],item_id: comment_params[:item_id],user_id: current_user.id)
+    if @comment.save
+      redirect_to item_path
+    else
+      render: new
   end
 
+  def new
+  end
+  
+  private
   def comment_params
-    params.require(:comment).permit(:text).merge(item_id: params[:id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id,item_id: params[:id])
   end
 end
