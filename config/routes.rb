48db3a79475ace2f5467e9ef180baf413a46c842
zare926 <ilove.boc926@gmail.com>
2020-04-04 17:01:46 +0900

    serveritemsshow17

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items do
+  resources :items, only: [:index, :show] do
     resources :comments
     collection do
       get 'confirm'
