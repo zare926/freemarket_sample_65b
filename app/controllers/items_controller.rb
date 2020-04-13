308bea82bed7b12fae4df250389f4e0d0f754f11
yuichisan171 <lion.hanamaro@gmail.com>
2020-04-13 21:38:26 +0900

    編集4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,9 +14,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
