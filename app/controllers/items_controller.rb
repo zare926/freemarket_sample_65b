ab02650a78e5b329a052665d1a513a53d34e45b2
zare926 <ilove.boc926@gmail.com>
2020-04-19 18:48:33 +0900

    マージ修正のコミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,23 +12,9 @@ class ItemsController < ApplicationController
   
   def new
     @item = Item.new
-    @item.images.new
-    @category = Category.all.order("id ASC").limit(1)
-    
   end
 
   def create
-    @item = Item.new(item_params)
-    respond_to do |format|
-      format.html
-      format.json
-    end
-    binding.pry
-    if @item.save
-      redirect_to root_path and return
-    else
-      redirect_to new_item_path and return
-    end
   end
 
   def edit
@@ -50,29 +36,10 @@ class ItemsController < ApplicationController
   def confirm
   end
 
-  def get_category_children
-    @category_children = Category.find(params[:productcategory]).children
-  end
-
-  def get_category_grandchildren
-    @category_grandchildren = Category.find(params[:productcategory]).children
-  end
-
   private
 
   def item_params
-    params.require(:item).permit(
-                                  :name,
-                                  :description,
-                                  :brand,
-                                  :state,
-                                  :postage,
-                                  :prefecture,
-                                  :shipping_date,
-                                  :category_id,
-                                  :price,
-                                  :size,
-                                  images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id,status: 0)
+    params.require(:item).permit(:name, :description, :brand, :state, :status, :postage, :shipping_date, :category)
   end
 
   def category_params
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  belongs_to :user
   belongs_to :item
 
-  # validates :image,
-  #   presence: true
-  #   # presence: { message: "画像がありません" }
+  validates :image,
+    presence: true,
+    presence: { message: "画像がありません" }
 end
