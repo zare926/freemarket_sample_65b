e14e1b6e0d870b73d7b6510eac2f445ea559ba93
zare926 <ilove.boc926@gmail.com>
2020-04-19 18:35:08 +0900

    Revert "Merge pull request #49 from zare926/revert-46-register_login_server"
    This reverts commit 12c5e22ed387574a5ecea3a6d146be4d0fd68826, reversing
    changes made to 23daaca2c299b041aca321bdd2d71210d1c1ef46.

--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_142022) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.date "birthday"
+    t.date "birthday", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "email", default: "", null: false
     t.string "reset_password_token"
