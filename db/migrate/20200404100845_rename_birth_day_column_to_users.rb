900b5068b2af3b5916258217cd2b6a779d972384
yuichisan171 <lion.hanamaro@gmail.com>
2020-04-25 15:34:21 +0900

    編集9

--- db/migrate/20200404100845_rename_birth_day_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameBirthDayColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :birth_day, :birthday
-  end
-end
--- db/migrate/20200414140835_rename_src_column_to_images.rb
+++
@@ -1,5 +0,0 @@
-class RenameSrcColumnToImages < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :images, :src, :image
-  end
-end
