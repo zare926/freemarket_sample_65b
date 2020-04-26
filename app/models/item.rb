31cb1f69585f8385fe0cc1be7fd9b107a16e81d8
hikarikawa1028 <okmc7335@yahoo.co.jp>
2020-04-26 14:49:24 +0900

    acceptance　消去

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@ class Item < ApplicationRecord
 
   validates :status,
     presence: true,
-    acceptance: true,
     presence: { message: "選択してください" }
 
   validates :description,
@@ -14,32 +13,27 @@ class Item < ApplicationRecord
 
   validates :category_id,
     presence: true,
-    acceptance: true,
     presence: { message: "選択してください" }
 
   validates :state,
     presence: true,
-    acceptance: true,
     presence: { message: "選択してください" }
 
   validates :postage,
     presence: true,
-    acceptance: true,
     presence: { message: "選択してください" }
 
   # validates :prefecture,
   #   presence: true,
-  #   acceptance: true,
+  #
   #   presence: { message: "選択してください"}
 
   validates :shipping_date,
     presence: true,
-    acceptance: true,
     presence: { message: "選択してください" }
 
   validates :price,
     presence: true,
-    acceptance: true,
     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
     presence: { message: "must be less than or equal to 300" }
 
