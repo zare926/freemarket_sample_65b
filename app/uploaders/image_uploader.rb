721eb4e3a2d9fbf427651e99bc6668ba8b1f547a
hikarikawa1028 <okmc7335@yahoo.co.jp>
2020-04-27 16:26:10 +0900

    消えたコードの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [124, 124]
   
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
