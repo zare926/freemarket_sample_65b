d923c86587023650fd45c1c23be327dc10690599
yuichisan171 <lion.hanamaro@gmail.com>
2020-05-01 16:58:04 +0900

    commit1
    ・リファクタリングの他、nicknameの取得を記述。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_item, only: [:show, :signout, :payment]
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 
   def show
-  end
-
-  def signout
-  end
-
-  def payment
-    
+    @user = User.find(params[:id])
   end
 
   private
   def set_item
     @categories = Category.all
   end
-  
 end
